7ef2b25c430040a3f4486c14ae892d11c3751e60
asadawuth <taodewy@gmail.com>
2023-12-27 23:55:04 +0700

    finish step4 applayout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div>React-App</div>;
+  return (
+    <div class="todo">
+      <div class="todo__header">Header</div>
+      <div class="todo__sidebar">SideBar</div>
+      <div class="todo__content">TodoContent</div>
+    </div>
+  );
 }
 
 export default App;
